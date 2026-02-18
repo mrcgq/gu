74c54fb0d5c636c4c49e9a78b5927f2d03bae48d
mrcgq <pennymrs413@gmail.com>
2026-02-18 15:16:16 +0800

    Create ServerEditDialog.xaml.cs

---
+++ Dialogs/ServerEditDialog.xaml.cs
@@ -0,0 +1,183 @@
+// =============================================================================
+// 文件: Dialogs/ServerEditDialog.xaml.cs
+// 描述: 服务器编辑对话框代码
+// =============================================================================
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using PhantomGUI.Models;
+using PhantomGUI.Services;
+
+namespace PhantomGUI.Dialogs;
+
+public partial class ServerEditDialog : UserControl, INotifyPropertyChanged
+{
+    private ServerProfile _profile;
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+    
+    public ServerEditDialog(ServerProfile profile)
+    {
+        InitializeComponent();
+        DataContext = this;
+        
+        _profile = profile;
+        
+        // 复制属性
+        Name = profile.Name;
+        ServerAddress = profile.ServerAddress;
+        ServerPort = profile.ServerPort;
+        PSK = profile.PSK;
+        TransportMode = profile.TransportMode;
+        UploadMbps = profile.UploadMbps;
+        DownloadMbps = profile.DownloadMbps;
+        TLSEnabled = profile.TLSEnabled;
+        TLSServerName = profile.TLSServerName;
+        TLSFingerprint = profile.TLSFingerprint;
+        LocalSocksPort = profile.LocalSocksPort;
+        LocalHttpPort = profile.LocalHttpPort;
+        EnableARQ = profile.EnableARQ;
+        TimeWindow = profile.TimeWindow;
+    }
+    
+    // 属性绑定
+    private string _name = "";
+    public string Name
+    {
+        get => _name;
+        set { _name = value; OnPropertyChanged(); }
+    }
+    
+    private string _serverAddress = "";
+    public string ServerAddress
+    {
+        get => _serverAddress;
+        set { _serverAddress = value; OnPropertyChanged(); }
+    }
+    
+    private int _serverPort = 54321;
+    public int ServerPort
+    {
+        get => _serverPort;
+        set { _serverPort = value; OnPropertyChanged(); }
+    }
+    
+    private string _psk = "";
+    public string PSK
+    {
+        get => _psk;
+        set 
+        { 
+            _psk = value; 
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(UserId));
+        }
+    }
+    
+    private string _transportMode = "auto";
+    public string TransportMode
+    {
+        get => _transportMode;
+        set { _transportMode = value; OnPropertyChanged(); }
+    }
+    
+    private int _uploadMbps = 100;
+    public int UploadMbps
+    {
+        get => _uploadMbps;
+        set { _uploadMbps = value; OnPropertyChanged(); }
+    }
+    
+    private int _downloadMbps = 100;
+    public int DownloadMbps
+    {
+        get => _downloadMbps;
+        set { _downloadMbps = value; OnPropertyChanged(); }
+    }
+    
+    private bool _tlsEnabled;
+    public bool TLSEnabled
+    {
+        get => _tlsEnabled;
+        set { _tlsEnabled = value; OnPropertyChanged(); }
+    }
+    
+    private string _tlsServerName = "www.microsoft.com";
+    public string TLSServerName
+    {
+        get => _tlsServerName;
+        set { _tlsServerName = value; OnPropertyChanged(); }
+    }
+    
+    private string _tlsFingerprint = "chrome";
+    public string TLSFingerprint
+    {
+        get => _tlsFingerprint;
+        set { _tlsFingerprint = value; OnPropertyChanged(); }
+    }
+    
+    private int _localSocksPort = 1080;
+    public int LocalSocksPort
+    {
+        get => _localSocksPort;
+        set { _localSocksPort = value; OnPropertyChanged(); }
+    }
+    
+    private int _localHttpPort = 1081;
+    public int LocalHttpPort
+    {
+        get => _localHttpPort;
+        set { _localHttpPort = value; OnPropertyChanged(); }
+    }
+    
+    private bool _enableARQ = true;
+    public bool EnableARQ
+    {
+        get => _enableARQ;
+        set { _enableARQ = value; OnPropertyChanged(); }
+    }
+    
+    private int _timeWindow = 30;
+    public int TimeWindow
+    {
+        get => _timeWindow;
+        set { _timeWindow = value; OnPropertyChanged(); }
+    }
+    
+    // 计算属性：UserID
+    public string UserId => ConfigurationService.CalculateUserId(PSK);
+    
+    // 命令：生成 PSK
+    public void GeneratePSKCommand()
+    {
+        PSK = ConfigurationService.GeneratePSK();
+    }
+    
+    // 保存到 Profile
+    public void SaveToProfile()
+    {
+        _profile.Name = Name;
+        _profile.ServerAddress = ServerAddress;
+        _profile.ServerPort = ServerPort;
+        _profile.PSK = PSK;
+        _profile.TransportMode = TransportMode;
+        _profile.UploadMbps = UploadMbps;
+        _profile.DownloadMbps = DownloadMbps;
+        _profile.TLSEnabled = TLSEnabled;
+        _profile.TLSServerName = TLSServerName;
+        _profile.TLSFingerprint = TLSFingerprint;
+        _profile.LocalSocksPort = LocalSocksPort;
+        _profile.LocalHttpPort = LocalHttpPort;
+        _profile.EnableARQ = EnableARQ;
+        _profile.TimeWindow = TimeWindow;
+    }
+    
+    protected void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
+
+
+
+
